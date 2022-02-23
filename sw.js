acdb0a70b42aaadb70df778e81ebcf3a94563406
Mgjadhav11 <91558022+Mgjadhav11@users.noreply.github.com>
2022-02-23 16:27:37 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  'css/style.css',
+  'js/main.js'
+  ];
+
+/* Start the service worker and chche all of the app's content*/
+self.addEventListener('install',function(e){
+  e.waitUntil(
+    caches.open(cachName).then(function(cache){
+      return cache.addAll(filesToCache);
+    })
+    );
+});
+
+/* seve cached contenmt when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+    );
+});
